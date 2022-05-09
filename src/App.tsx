d01dff947356648c707f97668cc8a77ee8332b32
Godsheritage Adeoye <adeoyegodsheritage@gmail.com>
2022-05-09 15:18:42 +0100

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NewPlace from "./pages/NewPlace";
+import Users from "./pages/Users";
 
 const App = () => {
   return (
-    <div>lets start</div>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={<Users />} />
+        <Route path="/places/new" element={<NewPlace />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
