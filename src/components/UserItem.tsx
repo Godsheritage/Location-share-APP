f1e1c0a4432854f4e5ebb23533d1441ec38fc36d
Godsheritage Adeoye <adeoyegodsheritage@gmail.com>
2022-05-10 17:14:38 +0100

    worked on the ui elements

--- src/components/UserItem.tsx
+++ src/components/UserItem.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Card from "./shared/UIElements/Card";
 import Avatar from "./shared/UIElements/Avatar";
 
 const UserItem: React.FC<any> = ({ user }) => {
-
   // const image = {
   //   image : {user.image},
   //   alt : ''
@@ -11,17 +12,19 @@ const UserItem: React.FC<any> = ({ user }) => {
   // }
   return (
     <li className="user-item">
-      <div className="user-item__content">
-        <div className="user-item__image">
-          <Avatar infos={user} />
-        </div>
-        <div className="user-item__info">
-          <h2>{user.name}</h2>
-          <h3>
-            {user.places} {user.places === 1 ? "place" : "places"}{" "}
-          </h3>
-        </div>
-      </div>
+      <Card className="user-item__content">
+        <Link to={`/${user.id}/places`}>
+          <div className="user-item__image">
+            <Avatar infos={user} />
+          </div>
+          <div className="user-item__info">
+            <h2>{user.name}</h2>
+            <h3>
+              {user.places} {user.places === 1 ? "place" : "places"}{" "}
+            </h3>
+          </div>
+        </Link>
+      </Card>
     </li>
   );
 };
