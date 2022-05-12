155cb9fb52c372f3591957c29c885f63838f2aa9
Godsheritage Adeoye <adeoyegodsheritage@gmail.com>
2022-05-12 16:13:31 +0100

    updates

--- src/components/shared/navigtion/NavLinks.tsx
+++ src/components/shared/navigtion/NavLinks.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const NavLinks = () => {
   return (
-    <div>NavLinks</div>
+      <ul className="nav-links">
+          <li>
+              <NavLink to='/'>All users</NavLink>
+          </li>
+          <li>
+              <NavLink to='/'>my places</NavLink>
+          </li>
+          <li>
+              <NavLink to='/'>Add users</NavLink>
+          </li>
+          <li>
+              <NavLink to='/'>Authenticate</NavLink>
+          </li>
+      </ul>
   )
 }
 
