3cdc61b2f569d5935ee27f34bb6b9caf2f167b75
Godsheritage Adeoye <adeoyegodsheritage@gmail.com>
2022-05-13 15:33:56 +0100

    adding places page

---
+++ src/pages/UserPlaces.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UserPlaces = () => {
+  //todo this is going to fetch places from the backend then render 
+  
+    return (
+    <div>UserPlaces</div>
+  )
+}
+
+export default UserPlaces
