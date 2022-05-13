2bb912032dbe36da2ee80c29e215ceb35f4e9977
Godsheritage Adeoye <adeoyegodsheritage@gmail.com>
2022-05-13 16:21:41 +0100

    worked on place list styles

--- src/components/PlaceItem.tsx
+++ src/components/PlaceItem.tsx
@@ -1,8 +1,29 @@
 import React from "react";
+import Card from "./shared/UIElements/Card";
 import { placeItems } from "../types";
 
 const PlaceItem: React.FC<placeItems> = ({ placeItem }) => {
-  return <div>PlaceItem</div>;
+  return (
+    <li className="place-item">
+      <Card className="place-item__content">
+        <div className="place-item__image">
+          <img src="" alt="" className="src">
+            {placeItem.image}
+          </img>
+        </div>
+        <div className="place-item__info">
+          <h2>placeItem.title</h2>
+          <h3>placeItem.address</h3>
+          <p>placeItem.description</p>
+        </div>
+        <div className="place-item__actions">
+          <button>view on map</button>
+          <button>edit</button>
+          <button>delete</button>
+        </div>
+      </Card>
+    </li>
+  );
 };
 
 export default PlaceItem;
