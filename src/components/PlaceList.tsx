45559a25ff51185c737baf3f044f0f0f73a123e2
Godsheritage Adeoye <adeoyegodsheritage@gmail.com>
2022-05-13 15:41:27 +0100

    updates

--- src/components/PlaceList.tsx
+++ src/components/PlaceList.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import Card from "./shared/UIElements/Card";
 
-const PlaceList = () => {
-  return (
-    <div>PlaceList</div>
-  )
-}
+const PlaceList: React.FC<any> = ({ items }) => {
+  if (items.length === 0) {
+    <div className="place-list center">
+      <Card>
+        <h2>no places found maybe cretat one?</h2>
+        <button>share a place</button>  
+      </Card>
+    </div>;
+  }
+  return <div>PlaceList</div>;
+};
 
-export default PlaceList
+export default PlaceList;
