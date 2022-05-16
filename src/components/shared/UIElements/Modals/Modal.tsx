11d6ca4004abee5e791266763ceb302a8cb50a87
Godsheritage Adeoye <adeoyegodsheritage@gmail.com>
2022-05-16 13:15:56 +0100

    working on modal

--- src/components/shared/UIElements/Modals/Modal.tsx
+++ src/components/shared/UIElements/Modals/Modal.tsx
@@ -1,13 +1,37 @@
 import "./modal.css";
 import ReactDOM from "react-dom";
+import Backdrop from "../Backdrop";
+import { MotionValue } from "framer-motion";
 
-const ModalOverlay: React.FC<any> = ({ className }) => {
-  const content = <div className={`modal ${className}`}></div>;
+const ModalOverlay: React.FC<any> = (props) => {
+  const content = (
+    <div className={`modal ${props.className}`} style={props.style}>
+      <header className={`modal__header ${props.headerClass}`}>
+        <h2>{props.header}</h2>
+      </header>
+      <form
+        action=""
+        onSubmit={
+          props.onSubmit ? props.onSubmit : (event) => event.preventDefault()
+        }
+      >
+        <div className={`modal__content ${props.contentClass}`}>
+          {props.children}
+        </div>
+        <footer className={`modal__footer ${props.footerClass}`}>
+          {props.footer}
+        </footer>
+      </form>
+    </div>
+  );
   return ReactDOM.createPortal(content, document.querySelector("modal-hook")!);
 };
 
-const Modal: React.FC = () => {
-  return <div>Modal</div>;
+const Modal: React.FC<any> = (props) => {
+  return <>{props.show && <Backdrop onClick={props.onCancel}/>}
+  
+  
+  </>;
 };
 
 export default Modal;
