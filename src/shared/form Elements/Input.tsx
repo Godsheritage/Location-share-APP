a11b1ff8f7db4fced7413531f60290462d229ab7
Godsheritage Adeoye <adeoyegodsheritage@gmail.com>
2022-05-21 16:56:49 +0100

    input validtion

--- src/shared/form Elements/Input.tsx
+++ src/shared/form Elements/Input.tsx
@@ -1,12 +1,12 @@
-import React, {useReducer} from "react";
+import React, { useReducer } from "react";
 import "./input.css";
 
-const inputReducer = (state, action) => {
-  switch()
-}
-
-
-
+const inputReducer = (state: any, action: any) => {
+  switch (action.type) {
+    case "CHANGE":
+      return {};
+  }
+};
 
 const Input: React.FC<any> = ({ type, label, element, id }) => {
   const elements =
