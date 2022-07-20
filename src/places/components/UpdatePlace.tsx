e92c1921844d69905171ba20dd5c592faa40e92a
Godsheritage Adeoye <adeoyegodsheritage@gmail.com>
2022-07-20 05:30:02 +0100

    updates to the user place

--- src/places/components/UpdatePlace.tsx
+++ src/places/components/UpdatePlace.tsx
@@ -1,16 +1,35 @@
-import React from 'react'
-import { DUMMY_PLACES } from '../../users/pages/UserPlaces'
-import { useParams } from 'react-router-dom'
+import React from "react";
+import Input from "../../shared/form Elements/Input";
+import Button from "../../shared/form Elements/Button";
+import { useParams } from "react-router-dom";
+import { DUMMY_PLACES } from "../../users/pages/UserPlaces";
+import { VALIDATOR_REQUIRE } from "../../shared/util/validators";
 
 const UpdatePlace = () => {
-    const placeId = useParams().placeId
-    const identifiedPlace = DUMMY_PLACES.find((places) => places.id === placeId)
+  const placeId = useParams().placeId;
+  const identifiedPlace = DUMMY_PLACES.find((places) => places.id === placeId);
 
-    if
+  if (!identifiedPlace) {
+    return (
+      <div className="center">
+        <h2>no identified place found</h2>
+      </div>
+    );
+  }
+  return <div>
+    <Input
+    type="text"
+    id="title"
+    element="input"
+    label="Title"   
+validators={[VALIDATOR_REQUIRE()]}
 
-  return (
-    <div>UpdatePlace</div>
-  )
-}
+errorText="please enter a valid input"
+value={identifiedPlace.title}
+valid={true}
+onInput={()=>{}}
+    />
+  </div>;
+};
 
-export default UpdatePlace
+export default UpdatePlace;
