8146a18e40839a6a2aa294cb8c76d5ad3eb722b8
Godsheritage Adeoye <adeoyegodsheritage@gmail.com>
2022-07-21 21:03:37 +0100

    updates

--- src/places/components/PlaceList.tsx
+++ src/places/components/PlaceList.tsx
@@ -1,14 +1,15 @@
 import React from "react";
-import Card from "../../shared/UIElements/Card";
 import PlaceItem from "./PlaceItem";
 import { placeTypes } from "../../types";
+import Card from "../../shared/UIElements/Card";
+import Button from "../../shared/form Elements/Button";
 
 const PlaceList: React.FC<placeTypes > = ({ items }) => {
   if (items.length === 0) {
     <div className="place-list center">
       <Card>
         <h2>no places found maybe create one?</h2>
-        <button>share a place</button>  
+        <Button to='/places/new'>share a place</Button>  
       </Card>
     </div>;
   }
