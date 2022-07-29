c5dc4236f461ca889a5f9d879c1de7ec86c990b5
Godsheritage Adeoye <adeoyegodsheritage@gmail.com>
2022-07-29 06:38:46 +0100

    setting up the backend

--- server/dist/server.js
+++ server/dist/server.js
@@ -1,2 +1,11 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const http_1 = __importDefault(require("http"));
+const server = http_1.default.createServer();
+let port = process.env.PORT || 1234;
+server.listen(port, () => {
+    console.log(`app is listening on port ${port}`);
+});
---
+++ server/src/routes/places-routes.ts
@@ -0,0 +1,5 @@
+import express from 'express'
+
+const placesRoutes = express.Router()
+
+export default placesRoutes
