9732950b9434d8006c6339d574680fe81551c185
Godsheritage Adeoye <adeoyegodsheritage@gmail.com>
2022-07-30 21:17:42 +0100

    updates

--- server/dist/routes/places/places-controllers.js
+++ server/dist/routes/places/places-controllers.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.httpFetchPlacesByUserID = exports.httpFetchPlacesByPlaceId = void 0;
 const places_models_1 = require("../../models/places models/places-models");
+//FETCH THE PLACES BY ID
 const httpFetchPlacesByPlaceId = (req, res) => {
     const placeId = req.params.pid;
     const place = (0, places_models_1.getPlacesByPlaceId)(placeId);
@@ -11,9 +12,10 @@ const httpFetchPlacesByPlaceId = (req, res) => {
     return res.status(200).json(place);
 };
 exports.httpFetchPlacesByPlaceId = httpFetchPlacesByPlaceId;
+//to fetch the place by user ID
 const httpFetchPlacesByUserID = (req, res) => {
     const userId = req.params.uid;
-    const place = (0, places_models_1.getPlacesByPlaceId)(userId);
+    const place = (0, places_models_1.getPlacesByUserId)(userId);
     if (!place) {
         return res.status(404).json({ message: "could not find place" });
     }
--- server/src/routes/places/places-controllers.ts
+++ server/src/routes/places/places-controllers.ts
@@ -4,6 +4,7 @@ import {
   getPlacesByUserId,
 } from "../../models/places models/places-models";
 
+//FETCH THE PLACES BY ID
 export const httpFetchPlacesByPlaceId: RequestHandler = (req, res) => {
   const placeId: string = req.params.pid;
   const place = getPlacesByPlaceId(placeId)
@@ -13,9 +14,10 @@ export const httpFetchPlacesByPlaceId: RequestHandler = (req, res) => {
   return res.status(200).json(place);
 };
 
+//to fetch the place by user ID
 export const httpFetchPlacesByUserID: RequestHandler = (req, res) => {
   const userId = req.params.uid;
-  const place = getPlacesByPlaceId(userId)
+  const place = getPlacesByUserId(userId)
   if(!place){
     return res.status(404).json({message:"could not find place"})
   }
